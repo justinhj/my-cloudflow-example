c7f99b36fa631a3ff70d80a5cb5621636e78ada5
Justin Heyes-Jones <justinhj@gmail.com>
2020-08-11 11:12:47 -0700

    add test data script

---
+++ send-local-data.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for json in $(cat test-data/one-record-per-line.json)
+do
+  echo "Sending $json"
+  curl -i -X POST localhost:3000 -H "Content-Type: application/json" --data "$json"
+done
